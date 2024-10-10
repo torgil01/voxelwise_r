ec6a25c5df06b56d16317cc090db3a8f8d791460
Torgil Vangberg <torgil.vangberg@gmail.com>
2024-10-10 16:46:59 +0200

    add correct path and filenames

--- run_logistic.R
+++ run_logistic.R
@@ -25,23 +25,32 @@ run_logistic <- function() {
                       names_from = Time_point, 
                       values_from = c(Age,Sex)) %>%
     rename(Sex = Sex_1) %>%
-    select(-Sex_5) %>%
+    select(-Sex_5, -Age_5) %>%
     mutate(fw_path = "") %>%
-    mutate(wmh_path = "")
+    mutate(wmh_path = "") %>%
+    mutate(mask_path = "") %>% 
+    rename(age = Age_1) %>% 
+    rename(sex = Sex)
 
   # add paths for free water at tp = 1, and wmh at tp = 5
-  base_path="/aa/bb/cc"
+  base_path="/media/torgil/16ccecb7-2900-422e-8ea5-ff92c8e850dc/g100_fwe/data/mni_easyreg"
 
   n_cases <- dim(df.w)[1]
   fw_name <- "fwe_fw_volume_fraction_1.nii.gz"
   wmh_name <- "wmh_stackgen_cerebrum_5.nii.gz" 
+  mask_name <- "wm_mask_1.nii.gz"
   
   for (i in 1:n_cases) {
     df.w$fw_path[i] <- file.path(base_path,df.w$GMFID[i],fw_name)
-    df.w$wmh_path[i] <- file.path(base_path,df.w$GMFID[i],wmh_name)    
+    df.w$wmh_path[i] <- file.path(base_path,df.w$GMFID[i],wmh_name)
+    df.w$mask_path[i] <- file.path(base_path,df.w$GMFID[i],mask_name)
   }
   
-  df.train <- df.w %>% slice_sample(10)
+  
+  df.train <- df.w %>% slice_sample(n = 10)
+  
+  
+  
   
   return(df.train)
   
